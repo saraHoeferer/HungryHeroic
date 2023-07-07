467b0b69b28a7afa027fe82603c6528b50be8904
mervetuz <merve.tuz@stud.fh-campuswien.ac.at>
2023-07-07 18:12:52 +0200

    register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/authService/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  form: any = {
+    user_name: null,
+    user_mail: null,
+    user_password: null
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
 
+  constructor(private authService: AuthService) { }
+
+  onSubmit(): void {
+    const { user_name, user_mail, user_password } = this.form;
+
+    this.authService.register(user_name, user_mail, user_password).subscribe({
+      next: data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    });
+  }
 }
