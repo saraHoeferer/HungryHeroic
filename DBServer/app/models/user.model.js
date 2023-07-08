b9f48e05f0207ff872aa8185d54207567aa3f7f9
mervetuz <merve.tuz@stud.fh-campuswien.ac.at>
2023-07-09 00:32:53 +0200

    userid

--- DBServer/app/models/user.model.js
+++ DBServer/app/models/user.model.js
@@ -1,5 +1,9 @@
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define("users", {
+    user_id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true
+    },
     user_name: {
       type: Sequelize.STRING
     },
@@ -9,32 +13,10 @@ module.exports = (sequelize, Sequelize) => {
     user_password: {
       type: Sequelize.STRING
     }
-  },
-  {
+  },{
     timestamps: false
-  });
- return User;
+  }
+  );
+
+  return User;
 };
-
-// module.exports = (sequelize, Sequelize) => {
-//   const User = sequelize.define("users", {
-//     user_id: {
-//       type: Sequelize.INTEGER,
-//       primaryKey: true
-//     },
-//     user_name: {
-//       type: Sequelize.STRING
-//     },
-//     user_mail: {
-//       type: Sequelize.STRING
-//     },
-//     user_password: {
-//       type: Sequelize.STRING
-//     }
-//   },{
-//     timestamps: false
-//   }
-//   );
-
-//   return User;
-// };
